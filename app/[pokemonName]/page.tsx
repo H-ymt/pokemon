6f2d1625cf646cca982eff3ef70b22ecfa1fd52d
H-ymt <y.handai1272@gmail.com>
2024-03-16 20:41:25 +0900

    feat(pokemon name): Get pokemon's name in Japanese

--- app/[pokemonName]/page.tsx
+++ app/[pokemonName]/page.tsx
@@ -1,7 +1,6 @@
 import { PokemonImage } from "@/components/pokemon-image"
 import { Progress } from "@/components/ui/progress"
 import { getPokemon } from "@/lib/pokemonAPI"
-import Image from "next/image"
 
 export default async function PokemonPage({
   params,
--- components/pokemon-card.tsx
+++ components/pokemon-card.tsx
@@ -1,10 +1,40 @@
+import { useState, useEffect } from "react"
 import Link from "next/link"
+import axios from "axios"
 
 interface PokemonCardProps {
   name: string
 }
 
+const BASE_URL = "https://pokeapi.co/api/v2/"
+
+async function getPokemonJapaneseName(englishName: string) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}pokemon-species/${englishName.toLowerCase()}`
+    )
+    const data = response.data
+
+    for (const nameInfo of data["names"]) {
+      if (nameInfo["language"]["name"] == "ja-Hrkt") {
+        return nameInfo["name"]
+      }
+    }
+    return "日本語名が見つかりません。"
+  } catch (error) {
+    return "ポケモンの情報を取得できませんでした。"
+  }
+}
+
 export function PokemonCard({ name }: PokemonCardProps) {
+  const [japaneseName, setJapaneseName] = useState("")
+
+  useEffect(() => {
+    getPokemonJapaneseName(name).then((name) => {
+      setJapaneseName(name)
+    })
+  }, [name])
+
   return (
     <Link
       href={name}
@@ -12,7 +42,7 @@ export function PokemonCard({ name }: PokemonCardProps) {
       key={name + "Card"}
     >
       <h2 className={`text-2xl font-semibold`}>
-        {name.charAt(0).toUpperCase() + name.slice(1)}
+        {japaneseName.charAt(0).toUpperCase() + japaneseName.slice(1)}
       </h2>
     </Link>
   )
